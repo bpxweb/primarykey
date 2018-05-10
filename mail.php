2acfd1d3705e379b1ddc158bd92e9b9aa9a498c0
pat <patpjr@gmail.com>
2018-05-10 14:29:50 +0700

    test

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+$name=stripslashes($_POST["name"]);
+$email=stripslashes($_POST["email"]);
+$message=stripslashes($_POST["message"]);
+$secret="6LeatlUUAAAAAMcI2Bb37x_dHO3W05gq2S2m28Gt";
+$response=$_POST["captcha"];
+
+$verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}");
+$captcha_success=json_decode($verify);
+if ($captcha_success->success==false) {
+  //This user was not verified by recaptcha.
+	echo "1";
+
+}
+else if ($captcha_success->success==true) {
+  //This user is verified by recaptcha
+ echo "2";
+}
+
+
+?>
