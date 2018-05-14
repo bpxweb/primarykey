7726820a98d51800d03c22e14e50e5500a3f9752
pbxweb <patpjr@gmail.com>
2018-05-14 10:07:26 +0700

    update portfolio

--- homePage/homePortfolio.php
+++ homePage/homePortfolio.php
@@ -1,74 +1,102 @@
-    <section class="section-padding gray-bg" id="portfolio-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-sm-offset-3 text-center">
-                    <div class="page-title">
-                        <h2>Special team</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit voluptates, temporibus at, facere harum fugiat!</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-3">
-                    <div class="single-team">
-                        <div class="team-photo">
-                            <img src="images/team-section-1.png" alt="">
-                        </div>
-                        <h4>JEMY SEDONCE</h4>
-                        <h6>Co. Founder</h6>
-                        <ul class="social-menu">
-                            <li><a href="#"><i class="ti-facebook"></i></a></li>
-                            <li><a href="#"><i class="ti-twitter"></i></a></li>
-                            <li><a href="#"><i class="ti-linkedin"></i></a></li>
-                            <li><a href="#"><i class="ti-pinterest"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3">
-                    <div class="single-team">
-                        <div class="team-photo">
-                            <img src="images/team-section-2.png" alt="">
-                        </div>
-                        <h4>DEBORAH BROWN</h4>
-                        <h6>UX Designer</h6>
-                        <ul class="social-menu">
-                            <li><a href="#"><i class="ti-facebook"></i></a></li>
-                            <li><a href="#"><i class="ti-twitter"></i></a></li>
-                            <li><a href="#"><i class="ti-linkedin"></i></a></li>
-                            <li><a href="#"><i class="ti-pinterest"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3">
-                    <div class="single-team">
-                        <div class="team-photo">
-                            <img src="images/team-section-3.png" alt="">
-                        </div>
-                        <h4>HARRY BANKS</h4>
-                        <h6>Founder</h6>
-                        <ul class="social-menu">
-                            <li><a href="#"><i class="ti-facebook"></i></a></li>
-                            <li><a href="#"><i class="ti-twitter"></i></a></li>
-                            <li><a href="#"><i class="ti-linkedin"></i></a></li>
-                            <li><a href="#"><i class="ti-pinterest"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-3">
-                    <div class="single-team">
-                        <div class="team-photo">
-                            <img src="images/team-section-4.png" alt="">
-                        </div>
-                        <h4>VICTORIA CLARK</h4>
-                        <h6>Creative Director</h6>
-                        <ul class="social-menu">
-                            <li><a href="#"><i class="ti-facebook"></i></a></li>
-                            <li><a href="#"><i class="ti-twitter"></i></a></li>
-                            <li><a href="#"><i class="ti-linkedin"></i></a></li>
-                            <li><a href="#"><i class="ti-pinterest"></i></a></li>
-                        </ul>
-                    </div>
+<section class="section-padding gray-bg" id="portfolio-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3 text-center">
+                <div class="page-title">
+                    <h2>ผลงานของเรา</h2>
+                    <p>ส่วนหนึ่งของลูกค้าที่ไว้วางใจให้เราสร้างสรรผลงาน</p>
                 </div>
             </div>
         </div>
-    </section>
+        <!-- First Row -->
+        <div class="row">
+            <!-- 1 -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="single-team">
+                    <div class="team-photo">
+                        <img src="images/custom/mangomania" alt="">
+                    </div>
+                    <h4>Mango Mania</h4>
+                    <h6>www.mangomania.in.th</h6>
+                    <ul class="social-menu">
+                        <li class="social-menu-custom">
+                            <a target="_blank" href="https://www.mangomania.in.th">
+                                <i class="ti-share"></i>&nbsp;&nbsp; go to the website
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- 4 -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="single-team">
+                    <div class="team-photo">
+                        <img src="images/custom/popsiam.png" alt="">
+                    </div>
+                    <h4>POP Siam Golden Fruit</h4>
+                    <h6>www.popsiamgoldenfruit.com</h6>
+                    <ul class="social-menu">
+                        <li class="social-menu-custom">
+                            <a target="_blank" href="https://www.popsiamgoldenfruit.com">
+                                <i class="ti-share"></i>&nbsp;&nbsp; go to the website
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- 3 -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="single-team">
+                    <div class="team-photo">
+                        <img src="images/custom/sunmoon.png" alt="">
+                    </div>
+                    <h4>Sun Moon Desserts</h4>
+                    <h6>www.sunmoondesserts.com</h6>
+                    <ul class="social-menu">
+                        <li class="social-menu-custom">
+                            <a target="_blank" href="https://www.sunmoondesserts.com">
+                                <i class="ti-share"></i>&nbsp;&nbsp; go to the website
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- 5 -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="single-team">
+                    <div class="team-photo">
+                        <img src="images/team-section-1.png" alt="">
+                    </div>
+                    <h4>Fruitztory Dessert</h4>
+                    <h6>www.fruitztorydessert.com</h6>
+                    <ul class="social-menu">
+                        <li class="social-menu-custom">
+                            <a target="_blank" href="https://www.fruitztorydessert.com">
+                                <i class="ti-share"></i>&nbsp;&nbsp; go to the website
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- 2 -->
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="single-team">
+                    <div class="team-photo">
+                        <img src="images/team-section-1.png" alt="">
+                    </div>
+                    <h4>Richmond Fire</h4>
+                    <h6>www.richmondfire.de</h6>
+                    <ul class="social-menu">
+                        <li class="social-menu-custom">
+                            <a target="_blank" href="https://www.richmondfire.de">
+                                <i class="ti-share"></i>&nbsp;&nbsp; go to the website
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+        <!-- End of Second Row -->
+    </div>
+</section>
